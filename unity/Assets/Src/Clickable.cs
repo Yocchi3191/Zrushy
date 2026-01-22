74d6de912f078915946eac36bc0a4b1e14ceb38f
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-01-22 22:47:03 +0900

    クリック用のコンポーネントを作成
    実装は空なので.NET側が完了してunity側の作業が必要になったら実装する

---
+++ unity/Assets/Src/Clickable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// クリック入力の受け付け
+/// 入力に応じた反応の送信
+/// </summary>
+public class Clickable : MonoBehaviour
+{
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
