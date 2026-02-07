c06c5ba60234b699b2cc323289a4866e962d34ee
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-02-07 16:20:55 +0900

    シナリオ進行の設計変更
    - ScenarioDriverをクリックから一定時間で実行する方式に変更
    - シナリオ進行周りの設計ドキュメントを作成

--- unity/Assets/Scripts/Presentation/ScenarioDriver.cs
+++ unity/Assets/Scripts/Presentation/ScenarioDriver.cs
@@ -1,18 +1,75 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using System.Collections;
+using UnityEngine;
 using Zenject;
 using Zrushy.Core.Presentation;
 
+/// <summary>
+/// シナリオの自動進行を管理
+/// 将来的には IEvent からの通知で進行するが、現在は仮実装として一定時間ごとに進行
+/// </summary>
 public class ScenarioDriver : MonoBehaviour
 {
 	[Inject]
 	private ScenarioPlayer scenarioPlayer;
 
-	private void Update()
+	[SerializeField]
+	[Tooltip("シナリオ自動進行の間隔（秒）。0以下で自動進行無効")]
+	private float autoAdvanceInterval = 2.0f;
+
+	private Coroutine autoAdvanceCoroutine;
+
+	private void Start()
 	{
-		if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+		// ScenarioPlayer のイベントを購読
+		scenarioPlayer.OnScenarioStarted += StartAutoAdvance;
+		scenarioPlayer.OnScenarioFinished += StopAutoAdvance;
+	}
+
+	/// <summary>
+	/// シナリオの自動進行を開始
+	/// </summary>
+	public void StartAutoAdvance()
+	{
+		StopAutoAdvance();
+		if (autoAdvanceInterval > 0)
 		{
+			autoAdvanceCoroutine = StartCoroutine(AutoAdvanceCoroutine());
+		}
+	}
+
+	/// <summary>
+	/// シナリオの自動進行を停止
+	/// </summary>
+	public void StopAutoAdvance()
+	{
+		if (autoAdvanceCoroutine != null)
+		{
+			StopCoroutine(autoAdvanceCoroutine);
+			autoAdvanceCoroutine = null;
+		}
+	}
+
+	private IEnumerator AutoAdvanceCoroutine()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(autoAdvanceInterval);
+
+			// TODO: 将来的には IEvent からの通知で進行
+			// 例: IEventRepository.Subscribe(OnEventFired)
 			scenarioPlayer.Next();
 		}
+	}
+
+	private void OnDestroy()
+	{
+		// イベント購読を解除
+		if (scenarioPlayer != null)
+		{
+			scenarioPlayer.OnScenarioStarted -= StartAutoAdvance;
+			scenarioPlayer.OnScenarioFinished -= StopAutoAdvance;
+		}
+
+		StopAutoAdvance();
 	}
 }
