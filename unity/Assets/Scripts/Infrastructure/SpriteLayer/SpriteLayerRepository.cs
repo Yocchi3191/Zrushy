bbb4b7e8ea77e20455ef5ad911d6b0bca6b8bfca
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-03-18 15:16:59 +0900

    add: イラスト差し替え機能のcore実装を作成

---
+++ unity/Assets/Scripts/Infrastructure/SpriteLayer/SpriteLayerRepository.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Zrushy.Core.Domain.Sprite;
+
+namespace Zrushy.Core.Infrastructure.Unity.SpriteLayer
+{
+	public class SpriteLayerRepository : ISpriteLayerRepository
+	{
+		private readonly string resourceRootPath = "Heroin";
+		public string Get(SpriteLayerID id, ExpressionType type)
+		{
+			return $"{resourceRootPath}/{id.value}/{type.type}";
+		}
+	}
+}
