853a8de4f9fe73344880ab1e5f34249e5317237c
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-03-18 15:16:22 +0900

    update: PoC用シーンを各ディレクトリに移動
    delete: 使っていないファイルを削除

---
+++ unity/Assets/PoC/ChangeIllust/PoCButtons.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PoCButtons : MonoBehaviour
+{
+	//[SerializeField] private SomeDependency _dependency;
+
+	//public void OnButton1Click() => _dependency.SomeApi();
+	//public void OnButton2Click() => _dependency.OtherApi();
+}
