3728f1d4d2847705923f965c0a71513943f44437
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-03-20 22:39:36 +0900

    fix: ScenarioSelector.Selectがシナリオが見つからない場合に例外を返すよう修正

--- core/Zrushy.Core.Domain/Scenarios/Service/ScenarioSelector.cs
+++ core/Zrushy.Core.Domain/Scenarios/Service/ScenarioSelector.cs
@@ -1,13 +1,21 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
+using Zrushy.Core.Domain.Scenarios.Repository;
 using Zrushy.Core.Domain.Scenarios.ValueObject;
 
 namespace Zrushy.Core.Domain.Scenarios.Service
 {
     public class ScenarioSelector
     {
-        public static ScenarioID Select(ScenarioID[] scenarios) => scenarios[0];
+        public static ScenarioID Select(ScenarioID[] scenarios)
+        {
+            if (scenarios == null || scenarios.Length == 0)
+            {
+                throw new ScenarioNotFoundException("Scenarios array cannot be null or empty.");
+            }
+
+            return scenarios[0];
+        }
     }
 }
