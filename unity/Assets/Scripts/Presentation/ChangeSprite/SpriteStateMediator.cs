776e65fcfcc259ed437b908f013f447d61253fbe
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-04-01 21:40:56 +0900

    update: HoodieCordinaterの名前を変更

---
+++ unity/Assets/Scripts/Presentation/ChangeSprite/SpriteStateMediator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Zrushy.Core.Presentation.Unity.Assets.Scripts.Presentation.ChangeSprite;
+using Zrushy.Core.Presentation.Unity.ChangeSprite;
+
+namespace Zrushy.Core.Presentation.Unity
+{
+	public class SpriteStateMediator : MonoBehaviour, ISpriteStateTransitional
+	{
+		[SerializeField] SpriteState controller; // このSpriteStateの状態変化に応じて、dependentsのSpriteStateを変化させる
+		[SerializeField] SpriteState[] dependents; // controllerの状態に応じて、dependentsの遷移可能な状態が決まる
+		[SerializeField] ConstraintEntry[] constraints; // controllerの状態ごとの、dependentsの遷移可能な状態
+
+		void Start()
+		{
+
+		}
+
+		void OnDestroy()
+		{
+
+		}
+
+		private void OnZipperStateChanged(Sprite newState)
+		{
+
+		}
+	}
+}
