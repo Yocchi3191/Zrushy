9c746b9d813bf1577dee1cebae7698507b398d68
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-04-19 21:04:52 +0900

    feat: 服ずらし可不可判定機能 服エンティティを作成

---
+++ core/Zrushy.Core.Domain/Interactions/Entity/Clothing.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) yoshioyocchi314@gmail.com
+// Licensed under the MIT License.
+
+using System;
+using Zrushy.Core.Domain.Interactions.ValueObject;
+
+namespace Zrushy.Core.Domain.Interactions.Entity
+{
+    public class Clothing : IClothing
+    {
+        public ClothingID ID { get; init; }
+        private int _resistance;
+
+        public Clothing(ClothingID id, int resistance)
+        {
+
+            ID = id ?? throw new ArgumentNullException(nameof(id));
+
+            if (resistance < 0)
+                throw new ArgumentOutOfRangeException(nameof(resistance));
+
+            _resistance = resistance;
+        }
+
+        public bool CanPutOff(Affection affection, Arousal arousal) => affection.Value + arousal.Value >= _resistance;
+    }
+}
