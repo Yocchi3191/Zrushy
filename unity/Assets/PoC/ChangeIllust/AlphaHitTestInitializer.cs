a1f7298d18c3122bbcaad04c70d0185d97fbee81
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-05-05 13:10:47 +0900

    fix: Image.alphaHitTestThresholdが未設定になっていたのを修正

---
+++ unity/Assets/PoC/ChangeIllust/AlphaHitTestInitializer.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) yoshioyocchi314@gmail.com
+// Licensed under the MIT License.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zrushy.Presentation.ChangeIllust
+{
+    [RequireComponent(typeof(Image))]
+    public class AlphaHitTestInitializer : MonoBehaviour
+    {
+        void Start()
+        {
+            var image = GetComponent<UnityEngine.UI.Image>();
+            image.alphaHitTestMinimumThreshold = 0.1f;
+        }
+    }
+}
