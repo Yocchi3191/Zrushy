723a65255cb94bf72c7cf76b3a96352c8d09b77b
Yocchi3191 <yoshioyocchi314@gmail.com>
2026-05-17 16:33:51 +0900

    fix: 名前空間を追記

--- unity/Assets/PoC/ChangeIllust/YarnScenarioStartButton.cs
+++ unity/Assets/PoC/ChangeIllust/YarnScenarioStartButton.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using Yarn.Unity;
 
-public class YarnScenarioStartButton : MonoBehaviour
+namespace Zrushy.Core.Presentation.Debug
 {
-    [SerializeField] private DialogueRunner _dialogueRunner;
-    [SerializeField] private string _startNode = "Start";
+	public class YarnScenarioStartButton : MonoBehaviour
+	{
+		[SerializeField] private DialogueRunner _dialogueRunner;
+		[SerializeField] private string _startNode = "Start";
 
-    private void Start()
-    {
-        GetComponent<Button>().onClick.AddListener(OnClick);
-    }
+		private void Start()
+		{
+			GetComponent<Button>().onClick.AddListener(OnClick);
+		}
 
-    private void OnClick()
-    {
-        if (_dialogueRunner.IsDialogueRunning)
-            return;
+		private void OnClick()
+		{
+			if (_dialogueRunner.IsDialogueRunning)
+				return;
 
-        _dialogueRunner.StartDialogue(_startNode);
-    }
+			_dialogueRunner.StartDialogue(_startNode);
+		}
+	}
 }
